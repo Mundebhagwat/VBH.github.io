61597182862ade8ef03092774779156d3ec105a7
Mundebhagwat <123530941+Mundebhagwat@users.noreply.github.com>
2023-12-03 02:20:55 +0530

    Updated home page

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-// Make sure this code is not within a block scope or module
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
 
-function fadeOut() {
-  slides[currentSlide].style.opacity = 0;
-}
-
-function fadeIn() {
-  slides[currentSlide].style.opacity = 1;
-}
-
-function nextSlide() {
-  fadeOut();
-  setTimeout(() => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-    fadeIn();
-  }, 2000); // Adjust the time between fade out and fade in
-}
-
-setInterval(nextSlide, 5000);
 
 // code for burger
 const burger = document.querySelector('.burger');
